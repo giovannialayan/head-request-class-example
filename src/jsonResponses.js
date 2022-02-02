14efa68f8f6ac7df8d31c2db0bdbcdb1cb1e5d91
Giovanni Alayan (RIT Student) <gna7400@rit.edu>
2022-02-02 14:18:55 -0500

    head request example

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -2,31 +2,56 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
 
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const getUsersMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createdAt: Date.now(),
+  };
 
+  users[newUser.createdAt] = newUser;
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'the page you are looking for was not found',
+    id: 'notFound',
+  };
 
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSON(request, response, 404);
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -5,12 +5,34 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  'HEAD': {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  }
+};
+
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  console.dir(request.method);
+  console.dir(parsedUrl);
 
+  if(urlStruct[request.method] && urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  }
+  else {
+    urlStruct['GET'].notFound(request, response);
+  }
 };
 
-http.createServer(onRequest).listen(port);
-
-console.log(`Listening on 127.0.0.1: ${port}`);
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
